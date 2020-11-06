15b1e7a20ecaed40c312a180cf1a837a10dd0e0e
Uni-Mar <987601970@qq.com>
2020-11-06 15:13:02 +0800

    MyIO_add_some_arguments

--- myIO.py
+++ myIO.py
@@ -1,7 +1,7 @@
 '''
 Author: your name
 Date: 2020-11-01 18:35:16
-LastEditTime : 2020-11-03 12:09:59
+LastEditTime : 2020-11-06 11:06:55
 LastEditors  : UniMars
 Description: In User Settings Edit
 FilePath: \\python\\myPackage\\myIO.py
@@ -14,11 +14,19 @@ sys.path.append(r'E:/Programs/Code/python')
 import myPackage.myEncoding
 
 
-def my_write(path: str, text: str,encoding: str) -> None:
-    encoding = myPackage.myEncoding.judge_encoding(path)
-    if encoding != encoding:
-        myPackage.myEncoding.change_encoding(path, encoding, encoding)
-        encoding = encoding
-    with open(path, 'a+', encoding=encoding) as f:
+'''
+Description: write in files with your file path && text && encoding && mode in one function
+param {str} path
+param {str} text
+param {*} my_encoding
+param {*} mode
+return {*}
+'''
+def my_write(path: str, text: str,my_encoding='utf-8',mode='a+') -> None:
+    default_encoding = myPackage.myEncoding.judge_encoding(path)
+    if default_encoding != my_encoding:
+        myPackage.myEncoding.change_encoding(path, default_encoding, my_encoding)
+        default_encoding = my_encoding
+    with open(path, mode, encoding=my_encoding) as f:
         f.write(text)
     pass
